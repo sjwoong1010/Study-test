a06a8a17873760298630e418cf3ec0acb6cc809f
sjwoong1010 <sjwoong1010@hanmail.net>
2020-06-03 15:42:24 +0900

    init

---
+++ src/main/java/com/sangkon/tdd/Dollar.java
@@ -0,0 +1,14 @@
+package com.sangkon.tdd;
+
+public class Dollar {
+    public int amount;
+
+    public Dollar(int amount) {
+        this.amount = amount;
+    }
+
+    public Dollar times(int number) {
+        return new Dollar(this.amount * number);
+    }
+
+}
---
+++ src/test/java/com/sangkon/tdd/DollarTest.java
@@ -0,0 +1,17 @@
+package com.sangkon.tdd;
+
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class DollarTest {
+
+    @Test
+    public void testMultiplication() {
+        Dollar five = new Dollar(5);
+        Dollar product = five.times(2);
+        assertThat(product.amount).isEqualTo(10);
+        product = five.times(3);
+        assertThat(product.amount).isEqualTo(15);
+    }
+}
