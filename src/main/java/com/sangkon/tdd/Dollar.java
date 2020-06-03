4cd08e7755a578be79ac7e9bf5b1b255bf38f83c
sjwoong1010 <66340333+sjwoong1010@users.noreply.github.com>
2020-06-03 16:40:33 +0900

    tdd03 (#2)

--- src/main/java/com/sangkon/tdd/Dollar.java
+++ src/main/java/com/sangkon/tdd/Dollar.java
@@ -1,14 +1,30 @@
 package com.sangkon.tdd;
 
+import java.util.Objects;
+
 public class Dollar {
     public int amount;
 
     public Dollar(int amount) {
+
         this.amount = amount;
     }
 
     public Dollar times(int number) {
+
         return new Dollar(this.amount * number);
     }
 
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Dollar dollar = (Dollar) o;
+        return amount == dollar.amount;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(amount);
+    }
 }
--- src/test/java/com/sangkon/tdd/DollarTest.java
+++ src/test/java/com/sangkon/tdd/DollarTest.java
@@ -4,14 +4,20 @@ import org.junit.jupiter.api.Test;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
+
 public class DollarTest {
 
     @Test
     public void testMultiplication() {
         Dollar five = new Dollar(5);
         Dollar product = five.times(2);
-        assertThat(product.amount).isEqualTo(10);
+        assertThat(product).isEqualTo(new Dollar(10));
         product = five.times(3);
-        assertThat(product.amount).isEqualTo(15);
+        assertThat(product).isEqualTo(new Dollar(15));
+    }
+    @Test
+    public void testEquality() {
+        assertThat(new Dollar(5)).isEqualTo(new Dollar(5));
+        assertThat(new Dollar(5)).isNotEqualTo(new Dollar(6));
     }
 }
